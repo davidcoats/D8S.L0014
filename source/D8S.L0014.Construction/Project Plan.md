e8d612d5669a25c87ffc0df4012fdfebc3131146
davidcoats <david@getsafetycone.com>
2024-10-16 11:51:19 -0700

    Initial commit.

---
+++ source/D8S.L0014.Construction/Project Plan.md
@@ -0,0 +1,2 @@
+# D8S.L0014.Construction
+Construction console project for the D8S.L0014 library.
---
+++ source/D8S.L0014/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace D8S.L0014
+{
+	/// <summary>
+	/// An instance descriptor types library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
